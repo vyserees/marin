5797c328635b5af632e1d95e9188efeb67e62654
Vladimir Veljkovic <vyserees@gmail.com>
2016-04-13 13:30:41 +0200

    app corrections2

--- app.php
+++ app.php
@@ -16,14 +16,14 @@ function class_autoloader($class_name)
 {
     $name = strtolower($class_name);
     $possible_locations = array(
-        '/core/helpers/auth.php',
-        '/core/helpers/email.php',
-        '/core/helpers/paypal.php',
-        '/core/helpers/phpmailer.php',
-        '/core/helpers/pop3.php',
-        '/core/helpers/query.php',
-        '/core/helpers/smtp.php',
-        '/core/helpers/tables.php'
+        'core/helpers/auth.php',
+        'core/helpers/email.php',
+        'core/helpers/paypal.php',
+        'core/helpers/phpmailer.php',
+        'core/helpers/pop3.php',
+        'core/helpers/query.php',
+        'core/helpers/smtp.php',
+        'core/helpers/tables.php'
     );
     foreach ($possible_locations as $location) {
         if (file_exists($location)) {
